cc5d35a827d6e456faea0ef71fe289e5f787af96
jerry2793 <67173020+jerry2793@users.noreply.github.com>
2020-11-25 19:31:37 -0800

    added basic model for forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Posts(models.Model):
+  user = models.ForegeinKey(User, models.CASCADE)
+  title = models.CharField(max_length=500,blank=False)
+  body = models.TextField(blank=False)
+  
+  
+class Comments(models.Model):
+  user = models.ForegeinKey(User, models.CASCADE)
+  post = models.ForegeinKey(Post,models.CASCADE)
+  body = models.TextField(blank=False)
+  
+  
+class Replies(models.Model):
+  user = models.ForegeinKey(User, models.CASCADE)
+  comment = models.ForegeinKey(Comments,models.CASCADE)
+  body= models.CharField(max_length=1000)
